e4cbfc9d2b9d02445aa617902d9ef386bcc67aeb
Marten Seemann <martenseemann@gmail.com>
2019-02-09 13:23:53 +0800

    reset the delivery method after calculating the prices

--- Application/Controller/PaymentController.php
+++ Application/Controller/PaymentController.php
@@ -2,10 +2,31 @@
 namespace SeemannIT\AllShippingCostsModule\Application\Controller;
 
 class PaymentController extends PaymentController_parent {
+  protected $_aDelCosts = null;
+
   public function getDeliveryCost($id) {
+    if($this->_aDelCosts == null) {
+      $this->_getDeliveryCosts();
+    }
+    return $this->_aDelCosts[$id];
+  }
+
+  protected function _getDeliveryCosts() {
     $oBasket = $this->getSession()->getBasket();
-    $oBasket->setShipping($id);
+    // save the old shipping id
+    $origShippingId = $oBasket->getShippingId();
+    
+    $delCosts = [];
+    foreach(array_keys($this->getAllSets()) as $id) {
+      $oBasket->setShipping($id);
+      $oBasket->calculateBasket(true);
+      $delCosts[$id] = $oBasket->getDeliveryCost();
+    }
+    
+    // reset to the old shipping id
+    $oBasket->setShipping($origShippingId);
     $oBasket->calculateBasket(true);
-    return $oBasket->getDeliveryCost();
+
+    $this->_aDelCosts = $delCosts;
   }
 }
